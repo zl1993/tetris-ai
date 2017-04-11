1e79b869180cc7c9f8aaa03f48409f26cb15d15f
tet54 <eethai@msn.com>
2017-04-11 19:36:41 +0800

    Added multithreading for multiple games, optimized weights, added/removed heuristics

--- PlayerSkeleton.java
+++ PlayerSkeleton.java
@@ -1,27 +1,84 @@
 import java.util.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
+import tetrisTraining.Gene;
+import tetrisTraining.PlayerSkeleton.GeneRunnable;
 public class PlayerSkeleton {
 
     private ArrayList<Move> movesList = new ArrayList<Move>();
-
+    
+    //global variable to keep track of score across all games (if running concurrently)
+    static int totalScore = 0;
+    static boolean RUN1000GAMES = false;
+    
     public static void main(String[] args) {
-        State s = new State();
-        new TFrame(s);
-        PlayerSkeleton p = new PlayerSkeleton();
-
-        // while it game is not lost, continue making next move
-        while (!s.hasLost()) {
-            s.makeMove(p.chooseMove(s, s.getNextPiece(), s.legalMoves()));
-            s.draw();
-            s.drawNext(0, 0);
-            try {
-                Thread.sleep(10);//change back to 300
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+        // run 100 games concurrently
+        if (RUN1000GAMES == true){
+            int GAMENUM = 1000;
+            ExecutorService executor = Executors.newFixedThreadPool(GAMENUM);
+            for(int i = 0; i < GAMENUM; i++){
+                Runnable gameRunnable = new GameRunnable(i);
+                executor.execute(gameRunnable);
             }
+            executor.shutdown();
+            // Wait until all threads are finish
+            while (!executor.isTerminated()) {
+            }
+            System.out.println("Total Lines Cleared over " + GAMENUM + "games: " + totalScore);
+            System.out.println("Average Lines Cleared over " + GAMENUM + "games: " + totalScore/GAMENUM); 
         }
-        System.out.println("You have completed " + s.getRowsCleared() + " rows.");
+        // run 1 game
+        else{
+            State s = new State();
+            new TFrame(s);
+            PlayerSkeleton p = new PlayerSkeleton();
+    
+            // while it game is not lost, continue making next move
+            while (!s.hasLost()) {
+                s.makeMove(p.chooseMove(s, s.getNextPiece(), s.legalMoves()));
+                s.draw();
+                s.drawNext(0, 0);
+                try {
+                    Thread.sleep(0);//change back to 300
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            System.out.println("You have completed " + s.getRowsCleared() + " rows.");
+        }
     }
+    
+    //for multithreading
+    public static class GameRunnable implements Runnable {
+        private final int gameNum;
+        
+        GameRunnable(int gameNum) {
+            this.gameNum = gameNum;
+        }
+ 
+        @Override
+        public void run() {
+            State s = new State();
+        //  new TFrame(s);
+            PlayerSkeleton p = new PlayerSkeleton();
+
+            // while it game is not lost, continue making next move
+            while (!s.hasLost()) {
+                s.makeMove(p.chooseMove(s, s.getNextPiece(), s.legalMoves()));
+//              s.draw();
+//              s.drawNext(0, 0);
+              try {
+                  Thread.sleep(0);//change back to 300
+              } catch (InterruptedException e) {
+                  e.printStackTrace();
+              }
+          }
+          System.out.println("Game " + gameNum + " completed " + s.getRowsCleared() + " rows.");
+          totalScore = totalScore + s.getRowsCleared();
+        }
+    }
+    
 
     // AI picks the next move
     public int chooseMove(State s, int nextPiece, int[][] legalMoves) {
@@ -67,27 +124,39 @@ public class PlayerSkeleton {
     // calculate move score based on overall heuristic, weighted between 4
     // heuristic components
     public double evalScore(Move move) {
-        // weights
- //       double[] weights = new double[5];
-        //0.1693034145407168, 0.9513071845199775, 0.0021463310003033815, 0.21962220394263915, 0.13458259658716057
-        double rowsClearedWeight = 0.1693034145407168;//0.760666;
-        double numHolesWeight = 0.9513071845199775;//0.35663;
-        double spaceUsedWeight = 0.0021463310003033815;
-        double heightVarWeight = 0.21962220394263915;
-        double maxHeightWeight = 0.13458259658716057;
-        double maxWellDepthWeight = 0.01;
-        double maxHeightDiffWeight = 0;
+        
+        // WEIGHTS
+        double rowsClearedWeight = 0.900283;
+        double numHolesWeight = 6.302734;
+        double numVerticalConnectedHolesWeight = 0.200001;
+        //double totalHeightWeight = 0.510066;
+        double heightVarWeight = 0.900192;
+        double maxHeightWeight = 0.800240; 
+        double totalWellDepthWeight = 1.301027;
+        double maxWellDepthWeight = 0.100021;
+        //double maxHeightDiffWeight = 0.192596;
+        double avgHeightDiffFromMeanWeight = 0.871003;
 
         // find next state after applying move
         NextState nextState = move.getStateView().makeMoveView(move.getOrient(), move.getSlot(), move.getPiece());
 
         // only calculate score if next move does not result in a loss
-        if (nextState.hasLost() == true)
-            return -1;
-        else
-            return rowsClearedWeight * evalRowsCleared(nextState) - numHolesWeight * evalNumHoles(nextState)
-                    - spaceUsedWeight * evalSpaceUsed(nextState) - heightVarWeight * evalHeightVar(nextState)
-                    - maxHeightWeight * evalMaxHeight(nextState) - maxWellDepthWeight * evalMaxWellDepth(nextState) - maxHeightDiffWeight * evalMaxHeightDifference(nextState);
+        // if loss, just assign it lowest possible score
+        if (nextState.hasLost() == true){
+            return -Double.MAX_VALUE;
+        }
+        else{
+            return  rowsClearedWeight * evalRowsCleared(nextState) 
+                    - numHolesWeight * evalNumHoles(nextState)
+                    - numVerticalConnectedHolesWeight * evalNumVerticalConnectedHoles(nextState)
+                    //- totalHeightWeight * evaltotalHeight(nextState) 
+                    - heightVarWeight * evalHeightVar(nextState)
+                    - maxHeightWeight * evalMaxHeight(nextState) 
+                    - totalWellDepthWeight * evalTotalWellDepth(nextState) 
+                    - maxWellDepthWeight * evalMaxWellDepth(nextState) 
+                    //- maxHeightDiffWeight * evalMaxHeightDifference(nextState)
+                    - avgHeightDiffFromMeanWeight * evalAverageHeightDifferenceFromMean(nextState);
+        }
     }
 
     // MAXIMIZE - because it is the game's objective
@@ -117,20 +186,42 @@ public class PlayerSkeleton {
         }
         return numOfHoles;
     }
+    
+    // MINIMIZE - because holes cannot be filled up
+    // no. of vertically connected holes heuristic component
+    // vertically connected hole is a series of unused squares along a column, with at least one used square above it
+    public int evalNumVerticalConnectedHoles(NextState nextState) {
+        // get the field and top arrays of the state after next move is applied
+        int[][] field = nextState.getStateView().getField();
+        int[] top = nextState.getStateView().getTop();
+        int numOfVerticalConnectedHoles = 0;
+
+        // for each column, count the number of connected holes below the top used
+        // space
+        for (int i = 0; i < State.COLS; i++) {
+            for (int j = top[i] - 1; j >= 0; j--) { // only check for spaces
+                                                    // BELOW the top used space
+                if (field[j][i] == 0 && field[j+1][i] != 0) { // only count if above is filled
+                    numOfVerticalConnectedHoles++;
+                }
+            }
+        }
+        return numOfVerticalConnectedHoles;
+    }
 
     // MINIMIZE - so there is more space to drop new pieces
-    // total space used heuristic component - total 'taken up' space
-    public int evalSpaceUsed(NextState nextState) {
+    // total height heuristic component - total 'unavailable' space
+    public int evalTotalHeight(NextState nextState) {
         // get the top array of the state after next move is applied
         int[] top = nextState.getStateView().getTop();
-        int spaceUsed = 0;
+        int totalHeight = 0;
 
         // find the sum of the heights of each column of the state after next
         // move applied
         for (int i = 0; i < State.COLS; i++) {
-            spaceUsed = spaceUsed + top[i];
+            totalHeight = totalHeight + top[i];
         }
-        return spaceUsed;
+        return totalHeight;
     }
 
     // MINIMIZE - to avoid deep wells
@@ -165,27 +256,72 @@ public class PlayerSkeleton {
         return maxHeight;
     }
     
-    // MINIMIZE - to keep max well depth low
-    // maximum well depth heuristic component
-    public int evalMaxWellDepth(NextState nextState){
-        // get the top array of the state after next move is applied
+    // MINIMIZE - to keep total well depth low
+    // total well depth heuristic component
+    // well is a column with both its adjacent columns higher than it by at least 2 blocks
+    // well depth is the height from that column to the height of the shorter higher adjacent column
+    public int evalTotalWellDepth(NextState nextState){
+        
         int[] top = nextState.getStateView().getTop();
-        int currWellDepth = 0;
+        int totalWellDepth = 0;
+
+        // check if 1st column is a well (does not have left adjacent column)
+        // check 2nd column - 1st column if at least 2
+        if (top[1] - top[0] >= 2) {
+            totalWellDepth = totalWellDepth + (top[1] - top[0]);
+        }
+        
+        // check if 10th column is a well (does not have right adjacent column)
+        // check 9th column - 10th column if at least 2
+        if (top[8] - top[9] >= 2) {
+            totalWellDepth = totalWellDepth + (top[8] - top[9]);
+        }
+
+        // check if 2nd to 9th columns are wells (have both left and right adjacent columns)
+        for (int i = 1; i < State.COLS - 1; i++) {
+            // take the minimum height diff between left and right adjacent columns
+            // and check if minimum height diff is at least 2 (to be considered well)
+            if (Math.min(top[i-1] - top[i], top[i+1] - top[i]) >= 2){
+                totalWellDepth = totalWellDepth + Math.min(top[i-1] - top[i], top[i+1] - top[i]);
+            }
+        }
+
+        return totalWellDepth;
+    }
+    
+    // MINIMIZE - to keep maximum well depth low
+    // maximum well depth heuristic component
+    // well is a column with both its adjacent columns higher than it by at least 2 blocks
+    // well depth is the height from that column to the height of the shorter higher adjacent column
+    public int evalMaxWellDepth(NextState nextState){
+        int[] top = nextState.getStateView().getTop();
         int maxWellDepth = 0;
 
-        // find the sum of the absolute differences in heights of every adjacent
-        // column of the state after next move applied
-        for (int i = 1; i < State.COLS; i++) {
-            currWellDepth = Math.abs(top[i] - top[i - 1]);
-            if (currWellDepth > maxWellDepth){
-                maxWellDepth = currWellDepth;
+        // check if 1st column is a well (does not have left adjacent column)
+        // check 2nd column - 1st column if at least 2
+        if (top[1] - top[0] >= 2 && top[1] - top[0] > maxWellDepth) {
+            maxWellDepth = top[1] - top[0];
+        }
+        
+        // check if 10th column is a well (does not have right adjacent column)
+        // check 9th column - 10th column if at least 2
+        if (top[8] - top[9] >= 2 && top[8] - top[9] > maxWellDepth) {
+            maxWellDepth = top[8] - top[9];
+        }
+
+        // check if 2nd to 9th columns are wells (have both left and right adjacent columns)
+        for (int i = 1; i < State.COLS - 1; i++) {
+            // take the minimum height diff between left and right adjacent columns
+            // and check if minimum height diff is at least 2 (to be considered well)
+            if (Math.min(top[i-1] - top[i], top[i+1] - top[i]) >= 2 && Math.min(top[i-1] - top[i], top[i+1] - top[i]) > maxWellDepth){
+                maxWellDepth = Math.min(top[i-1] - top[i], top[i+1] - top[i]);
             }
         }
         return maxWellDepth;
     }
     
-    // MINIMIZE - to keep height difference between max and min height low, further
-    // maximum height difference component
+    // MINIMIZE - to keep height difference between max and min height low
+    // maximum height difference heuristic component
     public int evalMaxHeightDifference(NextState nextState) {
         // get the top array of the state after next move is applied
         int[] top = nextState.getStateView().getTop();
@@ -203,22 +339,31 @@ public class PlayerSkeleton {
         }
         return maxHeight - minHeight;
     }
-
-    /*
-    public int fitnessFunction(double rowsClearedWeight, double numHolesWeight, double spaceUsedWeight,
-            double heightVarWeight, double maxHeightWeight) {
+    
+    // MINIMIZE - to keep average height difference from mean low
+    // average height difference from mean heuristic  component
+    public double evalAverageHeightDifferenceFromMean(NextState nextState) {
+        // get the top array of the state after next move is applied
+        int[] top = nextState.getStateView().getTop();
+        int totalHeight = 0;
+        double meanHeight = 0; 
+        double totalHeightDiffFromMean = 0;
+        double averageHeightDiffFromMean = 0;
         
-                //genetic algorithm variables
-        int totalLinesCleared = 0;
-        fitness
-
-        double a = rowsClearedWeight;
-        double b = numHolesWeight;
-        double c = spaceUsedWeight;
-        double d = heightVarWeight;
-        double e = maxHeightWeight;
+        // find the sum of the heights of each column of the state after next
+        // move applied
+        for (int i = 0; i < State.COLS; i++) {
+            totalHeight = totalHeight + top[i];
+        }
+        
+        meanHeight = (double) totalHeight / (double) State.COLS;
+        for (int i = 0; i < State.COLS; i++) {
+            totalHeightDiffFromMean =  Math.abs(top[i] - meanHeight);
+        }
+        averageHeightDiffFromMean = totalHeightDiffFromMean / (double) State.COLS;
+        
+        return averageHeightDiffFromMean;
     }
-    */
 
     // class for each possible tetris move
     private static class Move {
@@ -407,8 +552,7 @@ public class PlayerSkeleton {
             // check if game ended, if move results in a loss, return Score
             // object with no rows cleared
             if (height + pHeight[currPiece][orient] >= ROWS) {
-                lost = true;
-                return new NextState(field, top, lost, 0);
+                return new NextState(field, top, true, 0);
             }
 
             // IF NOT LOSS CONDITION, FIND STATE SCORE AFTER APPLYING CURRENT
@@ -453,7 +597,7 @@ public class PlayerSkeleton {
                     }
                 }
             }
-            return new NextState(field, top, lost, rowsCleared);
+            return new NextState(field, top, false, rowsCleared);
         }
     }
 
